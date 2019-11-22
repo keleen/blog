0c490b24e34bc425f58085d591e8381dc4d5fafa
zhangxd <zhangxd@shuzilm.cn>
2019-11-22 17:19:18 +0800

    1. 修改配置

--- markdown/.vuepress/config.js
+++ markdown/.vuepress/config.js
@@ -2,5 +2,12 @@ module.exports = {
   title: 'TT大魔王',
   description : '要写东西了',
   dest: 'docs',
-  base: '/blog'
+  base: '/blog',
+  themeConfig: {
+    nav: [
+      {text: '主页', link: '/'},
+      {text: '前端', link: '/'},
+      {text: '生活', link: '/'}
+    ]
+  }
 }
